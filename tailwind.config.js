05b3ef72e2bce1ca6d44910a1c2426e0c10d6943
galihsatrio <sgalih@gmail.com>
2022-07-31 21:43:50 +0700

    landing page - homer

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./public/**/*.{html,js}'],
+	theme: {
+		extend: {},
+		fontFamily: {
+			inter: ['Inter'],
+			libre: ['Libre Baskerville'],
+			exo: ['Exo 2']
+		}
+	},
+	plugins: [],
+}
